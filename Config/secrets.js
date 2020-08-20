90103b1e8ac324aba57bcd3f6cf148e9d9e2e3cd
GregoryArcentales <50056677+GregoryArcentales@users.noreply.github.com>
2020-08-20 16:07:12 -0500

    .

---
+++ Config/secrets.js
@@ -0,0 +1,8 @@
+module.exports = {
+    cloudinary:{
+        cloud_name:'nombre_cloudinary',
+        api_key: 'api_cloudinary',
+        api_secret: 'api_secret_cloudinary'
+    },
+    jwtSecret:'secret_jwt'
+}
--- config/secrets.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    cloudinary:{
-        cloud_name:'dksluyb9v',
-        api_key: '939794397898466',
-        api_secret: '2XOjQ2s6Ej_RPM6k94UscAnuCCY'
-    },
-    jwtSecret:'holaholaholaholaholaholaholaholaholahola10'
-}
